11a9e07dd341a5335267f7793261e352e5f10ce8
CyrielleGl <cyrielle.gallou@gmail.com>
2022-03-24 10:01:16 +0100

    base structure

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const path = require('path')
 
 const PORT = process.env.PORT || 5003
@@ -7,6 +8,8 @@ const app = express()
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 // Ecoute du serveur
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
