b4ef416f96a0b0bbe5281285c01d0eb6ab2144b4
CyrielleGl <cyrielle.gallou@gmail.com>
2022-03-24 10:57:49 +0100

    fetch and connect to API

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const fetch = require('node-fetch')
 const path = require('path')
 
 const PORT = process.env.PORT || 5003
 
 const app = express()
+
+const getAllPokemon = async () => {
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        const json = await res.json()
+        console.table(json.results)
+        return json
+    } catch(err) {
+        console.log(err)
+    }
+}
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
@@ -12,7 +24,10 @@ app.engine('.hbs', exphbs.engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
 app.get('/', (req, res) => 
-    res.render('home', { title: 'Home', subtitle: 'Ma home page'}))
+    {
+        const pokemons = getAllPokemon()
+        res.render('home', { title: 'Home', subtitle: 'Ma home page'})
+    })
 
 app.get('/:title', (req, res) => {
     const title = req.params.title
