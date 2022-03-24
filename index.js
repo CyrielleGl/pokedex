a074d142fb158b654264e76a2d9d204e001ae691
CyrielleGl <cyrielle.gallou@gmail.com>
2022-03-24 09:29:31 +0100

    add public directory and middleware

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const PORT = process.env.PORT || 5003
 
 const app = express()
 
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Ecoute du serveur
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+
