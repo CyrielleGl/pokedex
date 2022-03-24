e47a4fa36c669dc2904be232a41453bd7964993c
CyrielleGl <cyrielle.gallou@gmail.com>
2022-03-24 10:36:56 +0100

    params and hbs

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const app = express()
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+app.engine('.hbs', exphbs.engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
 app.get('/', (req, res) => 
     res.render('home', { title: 'Home', subtitle: 'Ma home page'}))
-app.get('/about', (req, res) => 
-    res.render('about', { title: 'About', subtitle: 'Ma about page'}))
+
+app.get('/:title', (req, res) => {
+    const title = req.params.title
+    res.render('about', { title, subtitle: `Ma page ${title}`})
+})
 
 // Ecoute du serveur
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
